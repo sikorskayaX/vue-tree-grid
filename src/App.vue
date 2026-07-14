dc2c8c6565ac680678801d8d880f9e80410b20fe
ksenia <sikorskaya02@gmail.com>
2026-07-14 09:45:29 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="app-root">
+    <h1>TreeStore</h1>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+}
+#app-root {
+  padding: 20px;
+}
+</style>
